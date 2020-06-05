0096698cebf551a8cf0871a6d3faaa83abae2aaa
Juan Albarracin <jalbarracin@een.com>
2020-06-05 21:23:02 +0200

    sub new branch

--- app/src/main/java/com/example/sub/Tool.java
+++ app/src/main/java/com/example/sub/Tool.java
@@ -4,9 +4,7 @@ public class Tool {
 
 
     public void hello() {
-        System.out.println("Hello Submodule");
-        System.out.println("Hello Branches");
-        System.out.println("Changed from main");
+        System.out.println("Sub New Branch");
     }
 
 }
